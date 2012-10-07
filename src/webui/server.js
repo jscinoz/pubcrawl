a4134587073c636fcaaf24ab492598b28e68a7de
Jack Coulter <jscinoz@jscinoz.org>
2012-10-07 20:39:54 +1100

    Serve up some stuff, innit

--- src/webui/server.js
+++ src/webui/server.js
@@ -1,4 +1,23 @@
-exports.start = function(app) {
-    // TODO - Shiny web frontend
-    app.logdebug("WebUI TODO");
+exports.start = function(haraka) {
+    var express = require("express"),
+        app = express(),
+        STATIC_DIR = __dirname + "/static";
+
+    app.configure(function() {
+        app.use(require("less-middleware")({src: STATIC_DIR, compress: true}));
+        app.use(express.static(STATIC_DIR));
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(app.router);
+    });
+
+    app.get("/", function(req, res) {
+        res.send("DICKS");
+    });
+
+    app.listen("3000");
+
+    haraka.logdebug("WebUI started");
 }
+
+    // TODO - Shiny web frontend
