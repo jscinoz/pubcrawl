f15df941dcdc88b874c7d8179798d05c99dc59f9
Jack Coulter <jscinoz@jscinoz.org>
2012-10-08 00:19:21 +1100

    Implemented some of the webui. Still very early stages

--- src/webui/server.js
+++ src/webui/server.js
@@ -1,23 +1,64 @@
-exports.start = function(haraka) {
-    var express = require("express"),
-        app = express(),
-        STATIC_DIR = __dirname + "/static";
-
-    app.configure(function() {
-        app.use(require("less-middleware")({src: STATIC_DIR, compress: true}));
-        app.use(express.static(STATIC_DIR));
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
-        app.use(app.router);
-    });
-
-    app.get("/", function(req, res) {
-        res.send("DICKS");
-    });
-
-    app.listen("3000");
-
-    haraka.logdebug("WebUI started");
+function renderError(err, res) {
+    res.send(err);
 }
 
-    // TODO - Shiny web frontend
+exports.start = function(app) {
+    var express = require("express"),
+        webui = express(),
+        db = app.server.notes.db,
+        List = db.model("List", require("./../schema/List")),
+        STATIC_DIR = __dirname + "/static";
+
+    webui.configure(function() {
+        webui.set("views", __dirname + "/views");
+        webui.set("view engine", "jade");
+        webui.use(require("less-middleware")({src: STATIC_DIR, compress: true}));
+        webui.use(express.static(STATIC_DIR));
+        webui.use(express.bodyParser());
+        webui.use(express.methodOverride());
+        webui.use(webui.router);
+    });
+
+    webui.get("/", function(req, res) {
+        List.find(function(err, lists) {
+            if (err) return renderError(err, req); 
+
+            res.render("index", {
+                title: "Pubcrawl",
+                lists: lists
+            });
+
+        });
+    });
+
+    webui.get("/createList", function(req, res) {
+        res.render("createList", {
+            title: "Pubcrawl"
+        });
+    });
+
+    webui.post("/createList", function(req, res) {
+        // TODO: request validation
+
+        var params = req.body,
+            list = new List({
+                name: params.listName,
+                displayName: params.displayName,
+                description: params.description,
+                moderated: params.moderated === "true"
+            });
+
+        list.save(function(err, list) {
+            if (err) return renderError(err, req); 
+
+            res.render("listCreated", {
+                title: "Pubcrawl",
+                list: list
+            });
+        });
+    });
+
+    webui.listen("3000");
+
+    app.logdebug("WebUI started");
+}
