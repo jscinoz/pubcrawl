380818089a686eb3d81a4ef338a36b6d8891cf71
Jack Coulter <jscinoz@jscinoz.org>
2012-10-09 17:05:44 +1100

    Defined constraints on "email" and "moderated" fields

--- src/schema/Subscriber.js
+++ src/schema/Subscriber.js
@@ -3,8 +3,19 @@ var mongoose = require("mongoose"),
     List = require("./List");
 
 var Subscriber = new Schema({
-    email: String,
-    moderated: Boolean
+      // TODO: Create proper email address type, with conveneince methods
+      // similar to Haraka's Address object
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        sparse: true
+    },
+    moderated: {type: Boolean, required: true, default: false}
+    // TODO: Need to define a field here to store confirmation status per list
+    // maybe bring back the SubscribedList type...
 });
 
 Subscriber.method("lists", function(cb) {
