280c9911c975b2c94e6da9e7ea71942a4bb0e363
Jack Coulter <jscinoz@jscinoz.org>
2012-12-10 16:18:27 +1100

    Noted intended moderation logic/flow in queue.js comments

--- src/hooks/queue.js
+++ src/hooks/queue.js
@@ -6,10 +6,18 @@ module.exports = function(next, conn, params) {
         list = conn.notes.list,
         user = conn.notes.user;
 
-    // TODO moderation logic:
-    //  * If list unmoderated & user unmoderated, post
-    //  * If either list or user is moderated, add to appropriate queue (could
-    //    be both)
+    // TODO implement moderation flow as follows:
+    // 1. Email received and passes rcpt and rcpt_ok, ending up here
+    // 2. Sender is globally moderated?
+    //    true: Add message to user moderation queue
+    //      upon approval: pass message
+    //      upon rejection: send rejection message with reason(s) why
+    //    false: pass message
+    // 3. Recipient list is moderated?
+    //    true: Add message to list moderation queue
+    //      upon approval: pass message
+    //      upon rejection: send rejection message with reason(s) why
+    //    false: pass message
     message.header.lines().forEach(function(x) {
         app.logdebug("XXX: " + x);
     }, this);
