96223314029ae009f7bebb02ade3938bcce7fc07
Jack Coulter <jscinoz@jscinoz.so>
2013-01-06 17:57:41 +1100

    Include request IP in subscription confirmation

--- app/routes/list.js
+++ app/routes/list.js
@@ -6,6 +6,8 @@ var Q = require("q"),
     List = mongoose.model("List", require("../../lib/schema/List")),
     Subscriber = mongoose.model("Subscriber",
                                 require("../../lib/schema/Subscriber")),
+    Subscription = mongoose.model("Subscription",
+                                require("../../lib/schema/Subscription")),
     renderError = require("../util").renderError;
 
 exports.create = function(req, res) {
@@ -48,103 +50,103 @@ exports.subscribe = function(req, res) {
     
     // TODO: request validation
     Q.ninvoke(Subscriber, "findOne", {"email": params.email})
-        .then(function(subscriber) {
-            if (!subscriber) {
-                logger.logdebug("Creating new subscriber with address " +
-                             params.email);
+    .then(function(subscriber) {
+        if (!subscriber) {
+            logger.logdebug("Creating new subscriber with address " +
+                         params.email);
 
-                subscriber = new Subscriber({
-                    email: params.email
-                });
+            subscriber = new Subscriber({
+                email: params.email
+            });
 
-                return Q.ninvoke(subscriber, "save")
-                    .then(function() {
-                        return subscriber;
-                    });
-            }
+            return Q.ninvoke(subscriber, "save")
+            .then(function() {
+                return subscriber;
+            });
+        }
 
-            logger.logdebug("Found existing subscriber with address " +
-                            params.email);
+        logger.logdebug("Found existing subscriber with address " +
+                        params.email);
 
+        return subscriber;
+    })
+    .then(function(subscriber) {
+        return Q.ninvoke(List, "findById", params.listId)
+        .then(function(list) {
+            return list.subscribe(subscriber, req.ip);
+        }).then(function() {
             return subscriber;
-        })
-        .then(function(subscriber) {
-            return Q.ninvoke(List, "findById", params.listId)
-                .then(function(list) {
-                    return list.subscribe(subscriber);
-                }).then(function() {
-                    return subscriber;
-                });
-        })
-        .then(function(subscriber) {
-            req.flash("successMsgHead", "Confirmation required");
-            req.flash("successMsgBody",
-                "A confirmation email has been sent to " +
-                subscriber.email + ". Please click the link in " +
-                "this email to confirm your subscription");
-
-            res.redirect("/");
-        })
-        .fail(function(err) {
-            if (err.name === "AlreadySubscribed") {
-                logger.logdebug("Duplicate subscription attempt for address " +
-                                err.subscriber.email + " to list " +
-                                err.list.name);
-
-                req.flash("errorMsgHead", "Already subscribed");
-                req.flash("errorMsgBody",
-                    "The email address " + err.subscriber.email +
-                    " is already subscribed to the list " +
-                    (err.list.displayName || err.list.name));
-                res.redirect("/");
-            } else {
-                renderError(res, err);
-            }
         });
+    })
+    .then(function(subscriber) {
+        req.flash("successMsgHead", "Confirmation required");
+        req.flash("successMsgBody",
+            "A confirmation email has been sent to " +
+            subscriber.email + ". Please click the link in " +
+            "this email to confirm your subscription");
+
+        res.redirect("/");
+    })
+    .fail(function(err) {
+        if (err.name === "AlreadySubscribed") {
+            logger.logdebug("Duplicate subscription attempt for address " +
+                            err.subscriber.email + " to list " +
+                            err.list.name);
+
+            req.flash("errorMsgHead", "Already subscribed");
+            req.flash("errorMsgBody",
+                "The email address " + err.subscriber.email +
+                " is already subscribed to the list " +
+                (err.list.displayName || err.list.name));
+            res.redirect("/");
+        } else {
+            renderError(res, err);
+        }
+    });
 };
 
 exports.unsubscribe = function(req, res) {
     var params = req.body;
 
     Q.ninvoke(Subscriber, "findOne", {"email": params.email})
-        .then(function(subscriber) {
-            if (!subscriber) {
-                throw new Error("Subscriber " + params.email +
-                                " not found");
-            }
+    .then(function(subscriber) {
+        if (!subscriber) {
+            throw new Error("Subscriber " + params.email +
+                            " not found");
+        }
 
-            logger.logdebug("Removing subscriber " + subscriber.email);
+        logger.logdebug("Removing subscriber " + subscriber.email);
 
-            return Q.ninvoke(List, "findById", params.listId)
-                .then(function(list) {
-                    return list.unsubscribe(subscriber)
-                        .then(function() {
-                            return [subscriber, list];
-                        });
-                });
-        })
-        .spread(function(subscriber, list) {
-            req.flash("successMsgHead", "Unsubscribe successful");
-            req.flash("successMsgBody",
-                subscriber.email + " has been successfully unsubscribed " +
-                "from list " + (list.displayName || list.name));
-
-            res.redirect("/");
-        })
-        .fail(function(err) {
-            if (err.name === "NoSuchSubscriber") {
-                logger.logdebug("Attempted to remove non-existant " +
-                    "subscriber " + err.subscriber.email + " from list " +
-                    (err.list.displayName || err.list.name));
-
-                req.flash("errorMsgHead", "No such subscriber");
-                req.flash("errorMsgBody",
-                    "The email address " + err.subscriber.email +
-                    " is not subscribed to the list " +
-                    (err.list.displayName || err.list.name));
-                res.redirect("/");
-            } else {
-                renderError(res, err);
-            }
+        return Q.ninvoke(List, "findById", params.listId)
+        .then(function(list) {
+            return list.unsubscribe(subscriber)
+            .then(function() {
+                return [subscriber, list];
+            });
         });
+    })
+    .spread(function(subscriber, list) {
+        req.flash("successMsgHead", "Unsubscribe successful");
+        req.flash("successMsgBody",
+            subscriber.email + " has been successfully unsubscribed " +
+            "from list " + (list.displayName || list.name));
+
+        res.redirect("/");
+    })
+    .fail(function(err) {
+        if (err.name === "NoSuchSubscriber") {
+            logger.logdebug("Attempted to remove non-existant " +
+                "subscriber " + err.subscriber.email + " from list " +
+                (err.list.displayName || err.list.name));
+
+            req.flash("errorMsgHead", "No such subscriber");
+            req.flash("errorMsgBody",
+                "The email address " + err.subscriber.email +
+                " is not subscribed to the list " +
+                (err.list.displayName || err.list.name));
+            res.redirect("/");
+        } else {
+            renderError(res, err);
+        }
+    });
 };
